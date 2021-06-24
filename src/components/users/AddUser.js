0c3273f86c34e2fff019fad9a542bdb07068960b
stdimitrov05 <stdimitrov8b@gmail.com>
2021-06-24 18:05:05 +0300

    add Files

---
+++ src/components/users/AddUser.js
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import axios from 'axios'
+import { useHistory } from "react-router-dom";
+
+const AddUser = () => {
+  let history = useHistory();
+  const [user, setUser] = useState({
+    name: "",
+    school:"",
+    email: "",  
+    phone: "",
+    website: ""
+  });
+
+  const { name, school, email, phone, website } = user;
+  const onInputChange = e => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = async e => {
+    e.preventDefault();
+    await axios.post("http://localhost:3003/users", user);
+    history.push("/");
+  };
+  return (
+    <div className="container">
+      <div className="w-75 mx-auto shadow p-5">
+        <h2 className="text-center mb-4">Add a student </h2>
+        <form onSubmit={e => onSubmit(e)}>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control form-control-lg"
+              placeholder="Enter Your Name"
+              name="name"
+              value={name}
+              onChange={e => onInputChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control form-control-lg"
+              placeholder="Enter Your School"
+              name="school"
+              value={school}
+              onChange={e => onInputChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="email"
+              className="form-control form-control-lg"
+              placeholder="Enter Your E-mail Address"
+              name="email"
+              value={email}
+              onChange={e => onInputChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control form-control-lg"
+              placeholder="Enter Your Phone Number"
+              name="phone"
+              value={phone}
+              onChange={e => onInputChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control form-control-lg"
+              placeholder="Enter Link to the race or name "
+              name="website"
+              value={website}
+              onChange={e => onInputChange(e)}
+            />
+          </div>
+          <button className="btn btn-primary btn-block">Add User</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default AddUser;
